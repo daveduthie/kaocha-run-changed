b8a67c8a29ec480039d9b698c9cc3276cd7c216f
David Duthie <dave.a.duthie@gmail.com>
2021-09-27 16:27:30 +0200

    add gitpod paraphernalia

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((clojure-mode . ((cider-clojure-cli-aliases . "dev:test:build:reveal:add-lib"))))
+((clojure-mode . ((cider-clojure-cli-aliases . "dev:reveal:add-lib"))))
--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,15 @@
 (ns user
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.tools.deps.alpha.repl :refer [add-libs]]))
+
+(defn load-all-deps []
+  (let [depsEdn (edn/read-string (slurp "deps.edn"))
+        allDeps (apply merge
+                       (:deps depsEdn)
+                       (map :extra-deps (vals (:aliases depsEdn))))]
+    {:deps allDeps
+     :result (add-libs allDeps)}))
 
 (comment
-  (require '[clojure.tools.deps.alpha.repl :refer [add-libs]])
-  (add-libs (:deps (edn/read-string (slurp "deps.edn"))))
-
+  (load-all-deps)
   ,)
